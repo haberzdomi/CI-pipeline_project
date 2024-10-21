824f5962ad4923f6be3759d0c4fb8aa555fba53f
Haberz, Dominik <dominik.haberz@student.tugraz.at>
2024-10-21 20:25:14 +0200

    Add detailed comments and docstrings for biotsavart calculation

--- biotsavart_asdex.py
+++ biotsavart_asdex.py
@@ -2,6 +2,11 @@
 # Clara Rinner 01137166
 #%%
 # program biotsavart
+"""
+Calculate the magnetic field components on a grid defined in the 
+file 'biotsavart.inp' using the Biot-Savart law. The output is 
+written to the file 'field.dat'.
+"""
 #
 from vvn_asdex import magfie
 #%%
@@ -11,35 +16,42 @@ x = [0.0, 0.0, 0.0]
 #
 #% facbc=1.0
 #
+# Get the input data, defining the discrete space for the magnetic field calculation.
 f1=open('biotsavart.inp','r')
-nr,np,nz=[int(data) for data in f1.readline().split()]
-rmin, rmax=[float(data) for data in f1.readline().split()]
-zmin, zmax=[float(data) for data in f1.readline().split()]
+nr,np,nz=[int(data) for data in f1.readline().split()] # number of grid points in r, phi, z direction
+rmin, rmax=[float(data) for data in f1.readline().split()] # min and max values for r
+zmin, zmax=[float(data) for data in f1.readline().split()] # min and max values for z
 f1.close()
 #
-L1i=1
+L1i=1 
 #
 pmin=0.0
 pmax=2*math.pi/L1i
 #
-hrad = (rmax - rmin)/(nr-1)
-hphi = (pmax - pmin)/(np-1)
-hzet = (zmax - zmin)/(nz-1)
+hrad = (rmax - rmin)/(nr-1) # step size in r direction
+hphi = (pmax - pmin)/(np-1) # step size in phi direction
+hzet = (zmax - zmin)/(nz-1) # step size in z direction
 #
 #%%
 f1=open('field.dat','w')
+# Write the input parameters for the magnetic field calculation to the output file.
 print(nr,np,nz,L1i,file=f1)
 print(rmin,rmax,file=f1)
 print(pmin,pmax,file=f1)
 print(zmin,zmax,file=f1)
+
+# Loop over the grid points and calculate the magnetic field components.
 for i in range(nr):
-    print(i+1,'/',nr)
-    x[0]=rmin+hrad*i
+    print(i+1,'/',nr) # print progress
+    # TODO: Dynamic creation of matrices is more time consuming (especailly for higher dimensions) than preallocating them
+    x[0]=rmin+hrad*i # r value for current grid point
     for j in range(np):
-        x[1]=pmin+hphi*j
+        x[1]=pmin+hphi*j # phi value for current grid point
         for k in range(nz):
-            x[2]=zmin+hzet*k
+            x[2]=zmin+hzet*k # z value for current grid point
+            # Calculate different properties of the magnetic field at the current grid point
             bmod,sqrtg,bder,hcovar,hctrvr,hcurl=magfie(x)
+            # Write the covariant components ([0]=r, [1]=phi, [2]=z) of the magnetic field to the output file.
             print(hcovar[0]*bmod,hcovar[1]*bmod/x[0],hcovar[2]*bmod,file=f1)
 f1.close()
 #
--- vvn_asdex.py
+++ vvn_asdex.py
@@ -1,48 +1,64 @@
 #%%
 import numpy as np
+# If ier is set to 1 the file fort.36 will not be created or written to and 
+# facb has to be given for the magnetic field scaling factor.
 ier=0
+# If PROP is set to False the function datw7xm will be called to read the coil 
+# data from the files cur_asd.dd and co_asd.dd. In the current version of this 
+# module PROP=True will cause an error in GBmodc. 
 PROP=False
-facbc=1.0
+# Magnetic field scaling factor
+facbc=1.0 
 #% facb=None
-
+facb=1
 def gbcoil(RI,fI,ZI):
     """
-    multiply data from GBmodc with facb
+    Get the magnetic field components and their derivatives from GBmodc and 
+    scale them with the factor facb. Also create the file fort.36 containing 
+    the magnetic field scaling factor (facb), total number of nodes for all 
+    coils (nnodc), radius (r_st), the toroidal angle (fi_st), the altitude (z_st) 
+    and magnetic field in r, phi and z-direction (br, b_f, bz).
     
     Args:
-        RI, fI, ZI - cylindrical coordinates of grid point where the magnetic field is to be determined
+        RI (float): big radius
+        fI (float): toroidal angle
+        ZI (float): altitude
 
     Returns:
-        BRI
-        BfI
-        BZI
-        BRRI
-        BRfI
-        BRZI            magnetic field details
-        BfRI
-        BffI
-        BfZI
-        BZRI
-        BZfI
-        BZZI
+        BRI (float): radial component fo the magnetic field
+        BfI (float): toroidal component of the magnetic field
+        BZI (float): axial component of the magnetic field
+        BRRI (float): derivative of BRI in radial direction
+        BRfI (float): derivative of BRI in toroidal direction
+        BRZI (float): derivative of BRI in axial direction
+        BfRI (float): derivative of BfI in radial direction
+        BffI (float): derivative of BfI in toroidal direction
+        BfZI (float): derivative of BfI in axial direction
+        BZRI (float): derivative of BZI in radial direction 
+        BZfI (float): derivative of BZI in toroidal direction 
+        BZZI (float): derivative of BZI in axial direction 
     """
 
     global facbc, nnodc
     global facb, ier
 
+    # Skip if ier is 1, otherwise set facb to facbc and create fort.36.
     if ier!=1:
         facb=facbc
         f36 = open('fort.36', 'w')
-
+    
     X=RI
     Y=ZI
-
+    
+    # Get the magnetic field components and its derivatives from GBmodc.
     BX,Bf,BY,BRR,BRf,BRZ,BfR,Bff,BfZ,BZR,BZf,BZZ = GBmodc(X,fI,Y)
 
+    # Skip if ier is 1, otherwise write some input parameters for the magnetic field calculation to fort.36.
     if ier==0:
         print(facb,'=facb',nnodc,'=nnodc', file=f36)
         print(X,'=r_st',fI,'=fi_st',Y,'=z_st', file=f36)
 
+    # Multiply the magnetic field components and its derivatives with a scaling factor.
     BRI=BX*facb
     BfI=Bf*facb
     BZI=BY*facb
@@ -56,6 +72,7 @@ def gbcoil(RI,fI,ZI):
     BZfI=BZf*facb
     BZZI=BZZ*facb
 
+    # Skip if ier is 1, otherwise write the magnetic field components to the file fort.36 and close it.
     if ier==0:
         print(BRI,'=br',BfI,'=bf',BZI,'=bz', file=f36)
         f36.close()
@@ -66,29 +83,34 @@ def gbcoil(RI,fI,ZI):
 
 def GBmodc(RI,fI,ZI):
     """
-    evaluate Biot-Savart integral
+    Calculate the magnetic field components and their derivatives by 
+    evaluating the Biot-Savart integral. The geometry and current values 
+    of the coil are read from the files cur_asd.dd and co_asd.dd by 
+    calling datw7xm.
 
     Args:
-        RI, fI, ZI - cylindrical coordinates of grid point where the magnetic field is to be determined
+        RI (float): big radius
+        fI (float): toroidal angle
+        ZI (float): altitude
 
     Returns:
-        BRI
-        BfI
-        BZI
-        BRRI
-        BRfI
-        BRZI            magnetic field details
-        BfRI
-        BffI
-        BfZI
-        BZRI
-        BZfI
-        BZZI
+        BRI (float): radial component fo the magnetic field
+        BfI (float): toroidal component of the magnetic field
+        BZI (float): axial component of the magnetic field
+        BRRI (float): derivative of BRI in radial direction
+        BRfI (float): derivative of BRI in toroidal direction
+        BRZI (float): derivative of BRI in axial direction
+        BfRI (float): derivative of BfI in radial direction
+        BffI (float): derivative of BfI in toroidal direction
+        BfZI (float): derivative of BfI in axial direction
+        BZRI (float): derivative of BZI in radial direction 
+        BZfI (float): derivative of BZI in toroidal direction 
+        BZZI (float): derivative of BZI in axial direction
     """
     # w7x version
 
     # 25.07.2012    npar,ncoil=10850,27
-    npar,ncoil=10850,16
+    npar,ncoil=10850,16 
     # 25.07.2012    file2='cur_it.dd'
     file2='cur_asd.dd'
     # 25.07.2012    file3='co_itm.dd'
@@ -100,6 +122,8 @@ def GBmodc(RI,fI,ZI):
     global nnodc
     # 19.05.2011
     global PROP, bfrt, NPR, K2, XO, YO, ZO, cur, nco
+    
+    # Call datw7xm to read the coil data from the files cur_asd.dd and co_asd.dd.
     if not PROP:
         PROP=True
 
@@ -116,14 +140,17 @@ def GBmodc(RI,fI,ZI):
         print('  w7x_g version')
         print('curco')
         print(curco)
-
+    
+    # Get cartesian coordinates of the grid points
     fd=fI
     rd=RI
     cosf=np.cos(fd)
     sinf=np.sin(fd)
-    Y=rd*sinf   #cartesian coordinates of grid point
+    Y=rd*sinf   
     X=rd*cosf
     Z=ZI
+    
+    # Initialize magntic field components and their derivatives.
     BX=0.0
     BY=0.0
     BZ=0.0
@@ -157,25 +184,34 @@ def GBmodc(RI,fI,ZI):
         BZXB=0.0
         BZYB=0.0
         # 25.12.2008 end
-        XMXC=X-XO[K0]  #difference of grid point and first coil point | r-r'[0]
+        
+        # Difference of grid point and first coil point | r-r'[0]| in x, y and z direction
+        XMXC=X-XO[K0]  
         YMYC=Y-YO[K0]
         ZMZC=Z-ZO[K0]
-
-        R1=np.sqrt(XMXC*XMXC+YMYC*YMYC+ZMZC*ZMZC)  #distance between grid point and first coil point | |r-r'[0]|
+        
+        # Total distance between grid point and first coil point | |r-r'[0]|
+        R1=np.sqrt(XMXC*XMXC+YMYC*YMYC+ZMZC*ZMZC)
+        # Normalize the differences for each direction by the total distance.
         OR1=1.0/R1
         R1X=XMXC*OR1
         R1Y=YMYC*OR1
         R1Z=ZMZC*OR1
-        for K in range(K1,K2):   # loops through the remaining coil points
-            AX=XO[K]-XO[K-1]  #difference between the current coil point and the previous one | l
-            XMXC=X-XO[K]        #difference between grid point and current coil point | r-r'[k]
+        
+        # Loop over remaining coil points
+        for K in range(K1,K2): 
+            # Ai (i=X,Y,Z) is the difference between the current coil point and the previous one in i'th direction | l
+            # iMiC (i=X,Y,Z) is the difference between grid point and current coil point in i'th direction | r-r'[k]
+            AX=XO[K]-XO[K-1]  
+            XMXC=X-XO[K]
             AY=YO[K]-YO[K-1]
             YMYC=Y-YO[K]
-
+            
             AZ=ZO[K]-ZO[K-1]
             ZMZC=Z-ZO[K]
-            ZPRA=AX*XMXC+AY*YMYC+AZ*ZMZC    #scalar product of l and r-r'[k]
-            R2=np.sqrt(XMXC*XMXC+YMYC*YMYC+ZMZC*ZMZC)  #distance between grid point and current coil point | |r-r'[k]|
+            ZPRA=AX*XMXC+AY*YMYC+AZ*ZMZC  # Scalar product of l and r-r'[k]
+            R2=np.sqrt(XMXC*XMXC+YMYC*YMYC+ZMZC*ZMZC)  # Total distance between grid point and current coil point | |r-r'[k]|
+            # Normalize the differences for each direction by the total distance.
             OR2=1.0/R2
             R2X=XMXC*OR2
             R2Y=YMYC*OR2
@@ -184,7 +220,7 @@ def GBmodc(RI,fI,ZI):
             R1PR2=R1+R2
             OR1PR2=1.0/R1PR2
 
-            if cur[K-1]!=0.0:   # if not first point of a coil
+            if cur[K-1]!=0.0:   # If not first point of a coil
 
                 OBCP=1.0/(R2*R1PR2+ZPRA)
                 FAZRDA=-R1PR2*OBCP*OR1*OR2
@@ -268,16 +304,21 @@ def GBmodc(RI,fI,ZI):
 #     def datw7xm(nparx):
 def datw7xm(neli):
     """
-    return data from co_asd.dd and save data from cur_asd.dd in global variable curco
+    Read the currents for each coil from cur_asd.dd and save them in the global 
+    variable curco and read the remaining coil data (see Returns) from co_asd.dd.
 
     Args:
-        neli - length of returned lists
+        neli (int): number of elements in the arrays XO, YO, ZO, cur and nco
     Returns:
-        XO, YO, ZO - coordinate lists of coil points
-        cur - 0 if last point in a coil, otherwise 1
-        nco - coil number
+        XO (array[float], shape=(neli,)): x-coordinates of the nodes for each coil.
+        YO (array[float], shape=(neli,)): y-coordinates of the nodes for each coil.
+        ZO (array[float], shape=(neli,)): z-coordinates of the nodes for each coil.
+        cur (array[float], shape=(neli,)): current factors for each node. 0 for the 
+                                           last node of a coil and 1 otherwise.
+        nco (array[int], shape=(neli,)): Indices corresponding to the coils for each node.
     """
 
+    # Initialize variables
     XO = np.empty(neli)
     YO = np.empty(neli)
     ZO = np.empty(neli)
@@ -295,27 +336,33 @@ def datw7xm(neli):
     global nnodc
     # 19.05.2011
 
+    # Open the files co_asd.dd and cur_asd.dd
     f1 = open(file3, 'r')
     # 19.05.2011
     f2 = open(file2, 'r')
+    # Get current values for each coil from cur_asd.dd.
     curco[:] = [float(data) for data in f2.read().split()]
     # 19.05.2011 end
-    nnod = int(f1.readline())
+    # Total number of nodes for all coils (number of points definig the coil geometry)
+    nnod = int(f1.readline()) 
     nnodc=nnod
 
+    # Raise error if the number of nodes exceeds the length of the returned arrays.
     if nnod>neli:
         print(nnod,'=nnod',neli,'=neli')
         raise Exception('nnod>neli, stop')
-
+    # Read the XO, YO and ZO position of the nodes for each coil, a current factor cur 
+    # which is 0 for the last node of a coil and 1 otherwise and the coil index nco.
     for knod in range(nnod):
         XO[knod],YO[knod],ZO[knod],cur[knod],nco[knod] = [convert(data)
             for convert, data in zip([float,float,float,float,int], f1.readline().split())]
-    cbc=cur[nnod-1]
+    cbc=cur[nnod-1] # Current factor for the last node of the last coil
 
-
+    # Raise error if the current factor for the last node of the last coil is not 0.
     if cbc!=0.0:
         raise Exception(str(cbc)+'=cbc~=0, stop')
-
+    
+    # Close files.
     print(nnod,'=nnod,  datw7x terminated ')
     f1.close()
     # 25.07.2012
@@ -323,79 +370,68 @@ def datw7xm(neli):
     # 25.07.2012 end
     return XO,YO,ZO,cur,nco
 
-#
-#
-#
+
+
+
 def magfie(x):
     """
-    Determine properties of the magnetic field at the coordinates x
+    Get the magnetic field components and their derivatives from gbcoil and use them to determine the below 
+    properties (returns).  
 
     Args:
-        x - cylindrical coordinates of grid point where the magnetic field is to be determined
-
+        x (List[float], size=3): cylindrical coordinates of grid point where the magnetic field is to be determined
+                                 x[0]=R (big radius), x[1]=phi (toroidal angle), x[2]=Z (altitude)
+    
     Returns:
-        bmod - magnetic field module in units of the magnetic code
-        sqrtg - square root of determinant of the metric tensor
-        bder - derivatives of the logarithm of the magnetic field module over coordinates
-        hcovar - covariant components of the unit vector of the magnetic field direction
-        hctrvr - contravariant components of this vector
-        hcurl - contravariant component of the curl of this vector
+        bmod (float): magnetic field module in units of the magnetic code
+        sqrtg (float): square root of determinant of the metric tensor
+        bder (array[float], shape=(3,)): derivatives of the logarithm of the magnetic field module over coordinates
+        hcovar (array[float], shape=(3,)): covariant components of the unit vector of the magnetic field direction
+        hctrvr (array[float], shape=(3,)): contravariant components the unit vector of the magnetic field direction
+        hcurl (array[float], shape=(3,)): contravariant component of the curl the unit vector of the magnetic field direction
     """
-    #
-    # Computes magnetic field module in units of the magnetic code  - bmod,
-    # square root of determinant of the metric tensor           - sqrtg,
-    # derivatives of the logarythm of the magnetic field module
-    # over coordinates                                - bder,
-    # covariant componets of the unit vector of the magnetic
-    # field direction                                 - hcovar,
-    # contravariant components of this vector                 - hctrvr,
-    # contravariant component of the curl of this vector        - hcurl
-    # Order of coordinates is the following: x[0]=R (big radius),
-    # x[1]=phi (toroidal angle), x[2]=Z (altitude).
-    #
-    #  Input parameters:
-    #        formal:  x            -    array of coordinates
-    #  Output parameters:
-    #        formal:  bmod
-    #               sqrtg
-    #               bder
-    #               hcovar
-    #               hctrvr
-    #               hcurl
-    #
-    #  Called routines:  GBhs,GBRZd
-    #
-    rbig=max(x[0],1.0e-12)  #determines mimimum possible r
-    #
+    
+    rbig=max(x[0],1.0e-12)  # Prevent division by zero with minimum big radius R
+    
     ######## computation of gb in cylindrical co-ordinates ########
     ri=rbig
     fii=x[1]
     zi=x[2]
 
     br,bf,bz,brr,brf,brz,bfr,bff,bfz,bzr,bzf,bzz=gbcoil(ri,fii,zi)
-
+    
     ########## end of gb computation ##########
-    bmod=np.sqrt(br**2+bf**2+bz**2)
-    sqrtg=rbig
+    
+    # For all following variable naming: 
+    # r - radial component, f - toroidal component, z - axial component
+    
+    bmod=np.sqrt(br**2+bf**2+bz**2) # magnetic field module
+    sqrtg=rbig # square root of determinant of the metric tensor
+    
+    # unit vectors of the magnetic field directions
     hr=br/bmod
     hf=bf/bmod
     hz=bz/bmod
-    #
+    
+    # derivatives of the logarythm of the magnetic field module over coordinates
     bder=np.empty(3)
     bder[0]=(brr*hr+bfr*hf+bzr*hz)/bmod
     bder[1]=(brf*hr+bff*hf+bzf*hz)/bmod
     bder[2]=(brz*hr+bfz*hf+bzz*hz)/bmod
-    #
+    
+    # covariant componets of the unit vector of the magnetic field direction
     hcovar=np.empty(3)
     hcovar[0]=hr
     hcovar[1]=hf*rbig
     hcovar[2]=hz
-    #
+    
+    # contravariant components of the unit vector of the magnetic field direction
     hctrvr=np.empty(3)
     hctrvr[0]=hr
     hctrvr[1]=hf/rbig
     hctrvr[2]=hz
-    #
+    
+    # contravariant component of the curl of the unit vector of the magnetic field direction
     hcurl=np.empty(3)
     hcurl[0]=((bzf-rbig*bfz)/bmod+
             hcovar[1]*bder[2]-hcovar[2]*bder[1])/sqrtg
@@ -403,6 +439,6 @@ def magfie(x):
             hcovar[2]*bder[0]-hcovar[0]*bder[2])/sqrtg
     hcurl[2]=((bf+rbig*bfr-brf)/bmod+
             hcovar[0]*bder[1]-hcovar[1]*bder[0])/sqrtg
-    #
+    
     return bmod,sqrtg,bder,hcovar,hctrvr,hcurl
-    #
+
