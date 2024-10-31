a01bdcf32eb945f7d23a07e33ff7a61dd1be02f3
Haberz, Dominik <dominik.haberz@student.tugraz.at>
2024-10-31 14:11:22 +0100

    Split 3rd order polynomial tests into 3 test and fix error terms

--- testing_intro/parametrized_testing/testNumericalIntegration/test_boole.py
+++ testing_intro/parametrized_testing/testNumericalIntegration/test_boole.py
@@ -4,9 +4,14 @@ from numericalIntegration import integrationMethods
 
 
 @pytest.mark.parametrize(
+    # Error term: -8/945 *h**7 * f_6th_derivative(xi), where h=(b-a)/n and f(xi)=max(f(x)), a<x<b. 
+    # The error term vanishes for polynomials up to 5th order because of the 6th derivative. 
+    # A very small numerical error term of 1e-10 is always added to the prediction to avoid floating point errors.
     "f,a,b,n,prediction,tol",
     [
-        [lambda x: x+x**2+x**3, 0, 1, 1, 13 / 12, 1e-10],
+        [lambda x: x, 0, 1, 1, 1 / 2, 1e-10],
+        [lambda x: x**2, 0, 1, 1, 1 / 3, 1e-10],
+        [lambda x: x**3, 0, 1, 1, 1 / 4, 1e-10],
         [lambda x: x**2 * np.exp(-(x**3)), 0, 1, 2, (np.e - 1) / (3 * np.e), 1e-5],
     ],
 )
@@ -26,8 +31,8 @@ def testIntegrationBooleParametrized(f, a, b, n, prediction, tol):
 #         assert np.abs(result - prediction[i]) < tol[i]
 
 
-
-## When we added the polynomials of second and third order to the test function x: x the rectangular and the trapezoidal
-## integration were no longer accurate. The new threshold were chosen according to the error functions of the
-## respective integration methods. The simpson and boole methods were exact for the third grade polynomial, 
-## since they only generate errors from the forth grade and higher.
+## Comment for all tests:
+## When we added the polynomials of second and third order, the rectangular and the trapezoidal integration were no 
+## longer accurate. The new thresholds were chosen according to the error term calculation of the respective integration 
+## methods (see tables in https://en.wikipedia.org/wiki/Newton%E2%80%93Cotes_formulas). The simpson and boole methods 
+## were exact for the third grade polynomial, since they only generate errors from the forth grade and higher.
--- testing_intro/parametrized_testing/testNumericalIntegration/test_simpson.py
+++ testing_intro/parametrized_testing/testNumericalIntegration/test_simpson.py
@@ -4,9 +4,14 @@ from numericalIntegration import integrationMethods
 
 
 @pytest.mark.parametrize(
+    # Error term: -1/90 *h**5 * f_4th_derivative(xi), where h=(b-a)/n and f(xi)=max(f(x)), a<x<b. 
+    # The error term vanishes for polynomials up to 3rd order because of the 4th derivative. 
+    # A very small numerical error term of 1e-10 is always added to the prediction to avoid floating point errors.
     "f,a,b,n,prediction,tol",
     [
-        [lambda x: x+x**2+x**3, 0, 1, 1, 13 / 12, 1e-10],
+        [lambda x: x, 0, 1, 1, 1 / 2, 1e-10],
+        [lambda x: x**2, 0, 1, 1, 1 / 3, 1e-10],
+        [lambda x: x**3, 0, 1, 1, 1 / 4, 1e-10],
         [lambda x: x**2 * np.exp(-(x**3)), 0, 1, 5, (np.e - 1) / (3 * np.e), 1e-5],
     ],
 )
