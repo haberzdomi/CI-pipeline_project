d5b042ffecc061a35998958bc4c4f9baaba2d454
haberzdomi <dominik.haberz@student.tugraz.at>
2025-01-29 10:19:16 +0100

    Add test again

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,6 @@ from matplotlib import rcParams
 import numpy as np
 from importlib.resources import files
 import pytest
-from helpers.reference_solutions import fourier_analysis
+from tests.helpers.reference_solutions import fourier_analysis
 
 print(files("biotsavart_modes"))
